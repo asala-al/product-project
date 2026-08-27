963d9def2bd4eeac10ea59e8efbf1bf4b1eb5ffe
asala <alghadbanasala@gmail.com>
2026-08-27 17:37:16 +0200

    feat(productForm):add elements and its style css and add button and its style css

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,1 +1,13 @@
+import style from './Button.module.css'
+
+function Button() {
+    
+  return (
+    <div>
+      <button className={style.button}>add</button>
+    </div>
+  )
+}
+
+export default Button
 
--- src/components/ProductForm/ProductForm.jsx
+++ src/components/ProductForm/ProductForm.jsx
@@ -1,9 +1,22 @@
+import Button from '../Button/Button'
+import style from './ProductForm.module.css'
 
 
 function ProductForm() {
   return (
    <div>
+     <div className={style.ProductForm}>
+        <h3>Add New Product</h3>
+    <div className={style.form}>
+      
+      <input type="text"  placeholder="enter product name"/>
+      <input type="text" placeholder="enter price"/>
+      <input type="text" placeholder="select category"/>
+      <input type="text" placeholder="enter image url" />
+      <Button/>
+    </div>
 
+    </div>
    </div>
   )
 }
