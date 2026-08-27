4b331907dc17ec3b764276fd13f69444238eb3fb
asala <alghadbanasala@gmail.com>
2026-08-27 20:41:21 +0200

    feat(ProductSearch):add dropdown

--- src/components/ProductSearch/ProductSearch.jsx
+++ src/components/ProductSearch/ProductSearch.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import style from './ProductSearch.module.css'
 
 function ProductSearch() {
+    const [product,setProduct]=useState()
   return (
     <div className={style.ProductSearch}>
       <h3>total product : </h3>
       <input type="search" name="" id="" placeholder='search product...'/>
-      <input type="text" placeholder='All products'/>
+      
+      <select  value={product} onChange={(e)=>setProduct(e.target.value)}>
+        <option value="" >all category</option>
+        <option value="clothes">clothes</option>
+        <option value="electronics">electronics</option>
+        <option value="books">books</option>
+        <option value="accessories">accessories</option>
+        <option value="shoes">shoes</option>
+      </select>
     </div>
   )
 }
